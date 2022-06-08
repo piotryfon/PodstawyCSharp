2209ed2616442c44dfabfc4044592a09f84ded7c
piotryfon <31383071+piotryfon@users.noreply.github.com>
2022-06-08 21:31:16 +0200

    -Null Operators

---
+++ NullOperators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NullOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string name = "Piotr";
+            string defaultName = name ?? "Stanger"; 
+            Console.WriteLine(defaultName);
+
+
+            List<int> numbers = null;
+
+            (numbers ??= new List<int>()).Add(5);
+            Console.WriteLine(string.Join(" ", numbers));  // output: 5
+
+
+            int? a = null;
+
+            numbers.Add(a ??= 0);
+            Console.WriteLine(string.Join(" ", numbers));  // output: 5 0
+            Console.WriteLine(a);  // output: 0
+
+            numbers?.Clear();
+            Console.WriteLine(numbers);
+        }
+    }
+}
